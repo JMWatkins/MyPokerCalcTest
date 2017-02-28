665df70757d9e30f8d1ad2cd26ba474ec27422a2
Max <jmwatki2@illinois.edu>
2017-02-28 20:04:57 +1100

    Almost Done. Working on CLI

--- src/MyHand.java
+++ src/MyHand.java
@@ -27,11 +27,12 @@ public class MyHand{
             highestVal = 0;
             for(int j = 0; j<hand.length; j++){
                 if (hand[j].number > highestVal && !arContains(highestIndex, j)){
-                    highest = j;
+                	highest = j;
+                	//System.out.println(">>"+hand[j].number + " at index: " +j);
                     highestVal = hand[j].number;
-                    arAdd(highestIndex, j);
                 }
             }
+            highestIndex = arAdd(highestIndex, highest);
         }
         return highestVal;   
     }
@@ -45,12 +46,16 @@ public class MyHand{
         return false;
     }
 
-    private static boolean arAdd(int[] ar, int num){
-        for (int integer: ar){
-            if (integer == -1)
-                integer = num;
+    private static int[] arAdd(int[] ar, int num){
+        for (int i = 0; i < ar.length;i++){
+            if (ar[i] == -1){
+                ar[i] = num;
+                //System.out.println(ar[i]);
+            	return ar;
+            } 
         }
-        return false;
+        System.out.println("no space to add to ar");
+        return null;
     }
 
     public int highestCard(){
@@ -58,13 +63,13 @@ public class MyHand{
     }
 
 
-        private boolean containsSuit(char c){
+ /*       private boolean containsSuit(char c){
         for(int i = 0; i < hand.length; i++){
             if (hand[i].suit == c)
                 return true;
         }
         return false;
-    }
+    }*/
 
     private boolean containsNumber(int in){
         for(int i = 0; i < hand.length; i++){
@@ -72,6 +77,15 @@ public class MyHand{
                 return true;
         }
         return false;
+    }
+    
+    private int matchingVals(int val){
+    	int ret = 0;
+    	for (Card c: hand){
+    		if (c.number == val)
+    			ret ++;
+    	}
+    	return ret;
     }
 
 // ----------------------- USEFULL STUFF ------------------------------
@@ -81,35 +95,103 @@ public class MyHand{
     /*  returns total value of a hand (rank of hand * 20)
         plus the value of the constituants of the hand
     */
-    public int getHandVal(){
+    public float getHandVal(){
         //check for straight
-        int ret = 0;
-
-        int duplicates = checkDuplicates();
-        if (duplicates > 0)
-            ret = duplicates;
-
-        int straight = checkStraight();
-        if (straight > 0 && straight > ret)
-            ret = straight;
-
-        int flush = checkFlush();
-        if (flush > 0 && flush > ret)
-            ret = flush;
-
+    	float ret = 100+(float)highestCard();
+    	//System.out.println("Highest Card: " + ret);
+    	
+        float duplicates = checkDuplicates();
+        float straight = checkStraight();
+        float flush = checkFlush();
+        
+        float max1 = Math.max(duplicates, straight);
+        ret = Math.max(flush, ret );
+        ret = Math.max(ret, max1);
+        
         return ret;
     }
 
-    private int checkDuplicates(){
+    private float checkDuplicates(){
+    	int fours =  0;
+    	int threes = 0;
+    	int [] twos = {-1,-1};
+    	int [] visited = {-1,-1,-1,-1,-1};
+    	
+    	for (Card c: hand){
+    		int duplicateVals = matchingVals(c.number);
+    		if (duplicateVals>1 && !arContains(visited,c.number) ){
+    			arAdd(visited, c.number);
+    			if(duplicateVals == 4)
+    				fours = c.number;
+    			else if(duplicateVals == 3){
+    				threes = c.number;
+    			}
+    			else{
+    				if(twos[0] == -1){
+    					twos[0] = c.number;
+    				}
+    				else if(twos[1] == -1){
+    					twos[1] = c.number;
+    				}
+    			}
+    		}
+    		
+    	}
+    	// Decide scoring
+    	if (fours > 0){
+    		return 800 + ((float)fours);
+    	}
+    	else if (threes > 0){
+    		if(twos[0] > 0){
+    			return 700 + ((float)threes ) + ((float)twos[0]/10f);
+    		}
+    		else{
+    			return 400 + ((float)threes );
+    		}
+    		
+    	}
+    	else if (twos[0] > 0 && twos[1] >0){
+    		return 300 + ((float)twos[0] )+((float)twos[1] ); 
+    	}
+    	else if (twos[0] > 0){
+    		return 200+((float)twos[0] );
+    	}
+    	
         return 0;
     }
 
-    private int checkStraight(){
+    private float checkStraight(){
+    	int hi = highestCard();
+    	if (containsNumber(hi-1) && containsNumber(hi-2) && containsNumber(hi-3) && containsNumber(hi-4)){
+    		boolean isFlush = true;
+    		char s = hand[0].suit;
+    		for(Card c: hand){
+    			if(c.suit != s)
+    				isFlush = false;
+    		}
+    		if (isFlush){
+    			if(hi == 14){
+    				return 1000;
+    			}
+    			else 
+    				return 900 + (float)hi ;
+    		}
+    		else
+    			return 500 +(float)hi ;
+    	}
         return 0;
     }
 
-    private int checkFlush(){
-        return 0;
+    private float checkFlush(){
+    	boolean isFlush = true;
+		char s = hand[0].suit;
+		for(Card c: hand){
+			if(c.suit != s)
+				isFlush = false;
+		}
+		if (isFlush)
+			return 600 + (float)highestCard() ;
+		return 0;
     }
 
 
@@ -139,8 +221,10 @@ public class MyHand{
             }
         }
         private int getVal(char number){
-            if((int) number > 48 || (int) number < 58)
-                return number;
+            if((int)number > 48 && (int)number < 58){
+            	//System.out.println(Character.getNumericValue(number));
+                return Character.getNumericValue(number);
+            }
             else if (number == 'T')
                 return 10;
             else if (number == 'J')
--- src/SortHands.java
+++ src/SortHands.java
@@ -1,6 +1,126 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class SortHands {
+
+
 	public static void main(String arg[]){
+
+		//Scanner in = new Scanner(System.in);
+		File file = new File("poker-hands.txt");
+		Scanner in;
+		try {
+			in = new Scanner(file);
+
+			int counter = 0;
+			boolean player1Active = true;
+			String[] newHand = new String[5];
+
+			ArrayList<MyHand> player1 = new ArrayList<MyHand>();
+			ArrayList<MyHand> player2 = new ArrayList<MyHand>();
+
+			while(in.hasNext()){
+				String input = in.next();
+				//System.out.println(input);
+				if (counter  < 5){
+					newHand[counter] = input;
+					counter ++;
+				}
+				if (counter >= 5){
+					counter = 0;
+					if (player1Active)
+						player1.add(new MyHand(newHand));
+					else
+						player2.add(new MyHand(newHand));
+					player1Active = !player1Active;
+				}
+				//System.out.print(".");
+			}
+			in.close();
+			//System.out.println("Hands built");
+
+			int scoreP1 = 0;
+			int scoreP2 = 0;
+
+			// possibility for threading if computation takes too long
+			// --------------- COMPARING HANDS -----------------
+			if (player1.size() != player2.size())
+				System.out.println("Not matching number of hands");
+			for (int i = 0; i<player1.size(); i++){
+				MyHand p1 = player1.get(i);
+				MyHand p2 = player2.get(i);
+
+				float p1S = p1.getHandVal();
+				float p2S = p2.getHandVal();
+				float[] print = {p1S, p2S};
+				//printHands(print);
+				
+				if (p1S > p2S){
+					scoreP1 ++;
+				}
+				else if (p1S < p2S){
+					scoreP2 ++;
+				}
+
+				else{
+					boolean winnerDecided = false;
+					counter = 1;
+					while (!winnerDecided){
+						if (p1.NthHighestCard(counter) > p2.NthHighestCard(counter)){
+							scoreP1 ++;
+							winnerDecided = true;
+						}
+						else if (p1.NthHighestCard(counter) < p2.NthHighestCard(counter)){
+							scoreP2 ++;
+							winnerDecided = true;
+						}
+						else if (counter>5){
+							System.out.println("All characters matched?!");
+						}
+						else{
+							counter++;
+						}
+					}
+				}
+				//System.out.println(scoreP1 + "/" + scoreP2);
+			}
+
+			System.out.println("Player 1 wins: " + scoreP1 + " times");
+			System.out.println("Player 2 wins: " + scoreP2 + " times");
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	// Troubleshoot tool;
+	public static void printHands(float[] in){
+		String ret [] = new String[2];
+		for (int i = 0; i<2; i++){
+			if (in[i]>=1000)
+				ret[i]="Royal Flush";
+			else if (in[i]>900)
+				ret[i]="Straight Flush";
+			else if (in[i]>800)
+				ret[i]="Four of a Kind";
+			else if (in[i]>700)
+				ret[i]="Full House";
+			else if (in[i]>600)
+				ret[i]="Flush";
+			else if (in[i]>500)
+				ret[i]="Straight";
+			else if (in[i]>400)
+				ret[i]="Three of a Kind";
+			else if (in[i]>300)
+				ret[i]="Two Pair";
+			else if (in[i]>200)
+				ret[i]="Pair";
+			else if (in[i]>100)
+				ret[i]="High Card";
+		}
 		
+		System.out.println(in[0] + "/" + in [1] + "   |   " + ret[0] + "/" +ret[1]);
 	}
 }
